a8d62b00794a1d975056281ff625add57abffaf5
Thomas <thomas.sinka@dcmail.ca>
2021-03-06 06:05:41 -0500

    implementing scriptable tiles

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,45 @@
+﻿//  https://www.youtube.com/watch?v=XIqtZnqutGg
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap map;
+
+    [SerializeField]
+    private List<TileData> tileDatas;
+
+    private Dictionary<TileBase, TileData> dataFromTiles;
+
+    private void Awake()
+    {
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+        foreach (var tileData in tileDatas)
+        {
+            foreach (var tile in tileData.tiles)
+            {
+                dataFromTiles.Add(tile, tileData);
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            TileBase clickedTile = map.GetTile(gridPosition);
+
+            float walkSpeed = dataFromTiles[clickedTile].walkSpeed;
+
+            print(string.Format("Walk speed on: {0} is {1}", clickedTile, walkSpeed));
+        }
+    }
+}
