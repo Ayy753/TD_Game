dbca693ac1d43b0df848041e8320a2c3d828efc8
Thomas <thomas.sinka@dcmail.ca>
2021-03-09 09:20:48 -0500

    Started implementing enemies

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject exitGate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitGate = GameObject.Find("Exit");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (exitGate != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, exitGate.transform.position, 1f * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemyPrefab;
+    GameObject entrance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entrance = GameObject.Find("Entrance");
+    }
+
+    /// <summary>
+    /// Spawns random enemies at the entrance
+    /// </summary>
+    [ContextMenu("spawnEnemies")]
+    public void SpawnEnemies()
+    {
+        Vector3 position;
+        if (entrance != null)
+        {
+            position = entrance.transform.position;
+            float offsetx;
+            float offsety;
+
+            for (int i = 0; i < 10; i++)
+            {
+                offsetx = Random.Range(-1f, 1f);
+                offsety = Random.Range(-1f, 1f);
+                GameObject.Instantiate(enemyPrefab, position + new Vector3(offsetx, offsety), new Quaternion());
+            }
+        }
+        else
+        {
+            Debug.LogError("Entrance is null");
+        }
+    }
+}
