9570a3944bd0c76afb25f2820b8ff4a10c18feb0
Thomas <thomas.sinka@dcmail.ca>
2021-03-09 03:53:46 -0500

    implemented tile selection by layer

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -9,12 +9,24 @@ using UnityEngine.Tilemaps;
 public class MapManager : MonoBehaviour
 {
     [SerializeField]
-    private Tilemap map;
+    private Tilemap groundLayer;
+    [SerializeField]
+    private Tilemap decoreLayer;
+    [SerializeField]
+    private Tilemap structureLayer;
 
     [SerializeField]
     private List<TileData> tileDatas;
 
     private Dictionary<TileBase, TileData> dataFromTiles;
+
+    public enum Layers
+    {
+        GroundLayer,
+        DecoreLayer,
+        StructureLayer
+    }
+
     private void Awake()
     {
         //  Link each tile type to their respective shared data attributes
@@ -28,22 +40,69 @@ public class MapManager : MonoBehaviour
         }
     }
 
+    public void Start()
+    {
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int gridPosition = map.WorldToCell(mousePosition);
-            TileBase clickedTile = map.GetTile(gridPosition);
-            float walkSpeed = dataFromTiles[clickedTile].walkSpeed;
-            print(string.Format("Walk speed on: {0} is {1}", clickedTile, walkSpeed));
+            Vector2 mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            TileBase tile;
+            for (int i = 0; i <= 2; i++)
+            {
+                tile = SelectTile(i, mouseposition);
+                if (tile != null)
+                {
+                    print(string.Format("layer {0}:{1}", i, tile.name));
+                }
+                else
+                {
+                    print(string.Format("Tile {0} is null", i));
+                }
+            }
+
+            tile = SelectTile((int)Layers.GroundLayer, mouseposition);
+            float walkspeed = dataFromTiles[tile].walkSpeed;
+            print(string.Format("walk speed on: {0} is {1}", tile, walkspeed));
         }
+    }
+
+    /// <summary>
+    /// Select a tile from a specified layer
+    /// </summary>
+    /// <param name="layer">Layer 0 = ground, 1 = decore, 2 = structure</param>
+    /// <param name="position">A world point</param>
+    /// <returns></returns>
+    private TileBase SelectTile(int layer, Vector2 position)
+    {
+        Tilemap selectLayer;
+
+        switch (layer)
+        {
+            case 0:
+                selectLayer = groundLayer;
+                break;
+            case 1:
+                selectLayer = decoreLayer;
+                break;
+            case 2:
+                selectLayer = structureLayer;
+                break;
+            default:
+                throw new ArgumentException("Layer must be 0, 1, or 2");
+        }
+
+        Vector3Int gridPosition = selectLayer.WorldToCell(position);
+        TileBase tile = selectLayer.GetTile(gridPosition);
+        return tile;
     }
 
     public float GetTileWalkSpeed(Vector2 worldPosition)
     {
-        Vector3Int gridPosition = map.WorldToCell(worldPosition);
-        TileBase tile = map.GetTile(gridPosition);
+        Vector3Int gridPosition = groundLayer.WorldToCell(worldPosition);
+        TileBase tile = groundLayer.GetTile(gridPosition);
 
         if (tile == null)
         {
@@ -51,7 +110,6 @@ public class MapManager : MonoBehaviour
         }
 
         float walkSpeed = dataFromTiles[tile].walkSpeed;
-
         return walkSpeed;
     }
 }
