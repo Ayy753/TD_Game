6ca3c285379d8f1045eb08f0b25c4b2a5c5bd267
Thomas <thomas.sinka@dcmail.ca>
2021-04-16 07:57:46 -0400

    confined tooltip within screen boundries

--- Assets/Scripts/ToolTip.cs
+++ Assets/Scripts/ToolTip.cs
@@ -4,10 +4,14 @@ public class ToolTip : MonoBehaviour
 {
     private Text tooltipText;
     private RectTransform tooltipBackground;
+    private RectTransform canvasRect;
+    private RectTransform rectTransform;
     void Start()
     {
         tooltipText = transform.Find("Text").GetComponent<Text>();
         tooltipBackground = transform.Find("Background").GetComponent<RectTransform>();
+        canvasRect = GameObject.Find("Canvas").GetComponent<RectTransform>();
+        rectTransform = transform.GetComponent<RectTransform>();
         gameObject.SetActive(false);
     }
 
@@ -31,9 +35,13 @@ public class ToolTip : MonoBehaviour
     /// </summary>
     private void TrackMouse()
     {
-        transform.position = Input.mousePosition;
+        //  Calculate the tooltip background size based on text size and padding
         float textPadding = 4f;
         tooltipBackground.sizeDelta = new Vector2(tooltipText.preferredWidth + textPadding * 2f, tooltipText.preferredHeight + textPadding * 2f);
+
+        //  Clamp tooltip within bounds of screen
+        Vector3 cursorPos = Input.mousePosition / canvasRect.localScale.x;
+        rectTransform.anchoredPosition = new Vector3(Mathf.Clamp(cursorPos.x, 0, canvasRect.rect.width - tooltipBackground.rect.width), Mathf.Clamp(cursorPos.y, 0, canvasRect.rect.height - tooltipBackground.rect.height));
     }
 
     public void SetCurrentString(string data)
