6fc8039b94e41fbaa36564cb4bbb11e7e68785ff
Thomas <thomas.sinka@dcmail.ca>
2021-06-04 04:51:15 -0400

    removed enemies in range list from splash projectile and it will now instantly damage anything that enters radius while it is detonating

--- Assets/Scripts/Projectiles/SplashProjectile.cs
+++ Assets/Scripts/Projectiles/SplashProjectile.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SplashProjectile : Projectile, IUnitRangeDetection {
 
     private bool IsDetonating = false;
-    private List<Enemy> enemiesInRange;
     private const float detonationTime = 0.75f;
     private const float splashRadius = 3f;
 
@@ -26,7 +25,6 @@ public class SplashProjectile : Projectile, IUnitRangeDetection {
 
     public override void Initialize(Vector3 startPos, Transform target) {
         base.Initialize(startPos, target);
-        enemiesInRange = new List<Enemy>();
         LastTargetPosition = target.position;
         IsDetonating = false;
 
@@ -41,45 +39,35 @@ public class SplashProjectile : Projectile, IUnitRangeDetection {
         IsDetonating = true;
         rangeCollider.gameObject.SetActive(true);
         yield return new WaitForSeconds(detonationTime);
-        Detonate();
+        gameObject.SetActive(false);
     }
 
-    private void Detonate() {
-        Debug.Log("Enemies in range of splash: " + enemiesInRange.Count);
 
-        foreach (Enemy enemy in enemiesInRange) {
-            int numDamageTypes = ProjectileData.damageTypesAndAmounts.Length;
-            float distance = Vector3.Distance(transform.position, enemy.transform.position);
-            float damageScale = ((splashRadius - distance) / splashRadius);
+    private void ApplySplashDamage(Enemy enemy) {
+        int numDamageTypes = ProjectileData.damageTypesAndAmounts.Length;
+        float distance = Vector3.Distance(transform.position, enemy.transform.position);
+        float damageScale = ((splashRadius - distance) / splashRadius);
 
-            Damage.DamageTypeAndAmount[] scaledDamageTypes = new Damage.DamageTypeAndAmount[numDamageTypes];
+        Damage.DamageTypeAndAmount[] scaledDamageTypes = new Damage.DamageTypeAndAmount[numDamageTypes];
 
-            //  Ensure enemy is still within range (if damage scale > 0)
-            if (damageScale > 0) {
-                for (int i = 0; i < numDamageTypes; i++) {
-                    scaledDamageTypes[i].amount = ProjectileData.damageTypesAndAmounts[i].amount * damageScale;
-                    scaledDamageTypes[i].type = ProjectileData.damageTypesAndAmounts[i].type;
-                    Debug.Log(string.Format("Applying scaled damage Type: {0}, Amount: {1}", scaledDamageTypes[i].type, scaledDamageTypes[i].amount));
-                }
-                Damage.ApplyDamage(enemy, scaledDamageTypes);
+        //  Ensure enemy is still within range (if damage scale > 0)
+        if (damageScale > 0) {
+            for (int i = 0; i < numDamageTypes; i++) {
+                scaledDamageTypes[i].amount = ProjectileData.damageTypesAndAmounts[i].amount * damageScale;
+                scaledDamageTypes[i].type = ProjectileData.damageTypesAndAmounts[i].type;
             }
+            Damage.ApplyDamage(enemy, scaledDamageTypes);
         }
-
-        gameObject.SetActive(false);
     }
 
     public void UnitEnteredRange(IUnit unit) {
         Debug.Log("unit entered range");
         if (unit.GetType() == typeof(Enemy)) {
-            enemiesInRange.Add((Enemy)unit);
+            ApplySplashDamage((Enemy)unit);
         }
     }
 
     public void UnitLeftRange(IUnit unit) {
-        Debug.Log("unit left range");
-        if (unit.GetType() == typeof(Enemy)) {
-            enemiesInRange.Remove((Enemy)unit);
-        }
     }
 
     public float GetRange() {
