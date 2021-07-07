2c24d322012a5a264f22dfc9d8e9e1e5685e9c5b
Thomas <thomas.sinka@dcmail.ca>
2021-07-07 00:09:23 -0400

    cleaned up and renamed the main DI installer

---
+++ Assets/MainInstaller.cs
@@ -0,0 +1,41 @@
+using System;
+using Zenject;
+
+public class MainInstaller : MonoInstaller {
+    public override void InstallBindings() {
+
+        //  Manager bindings
+        Container.Bind<AsyncProcessor>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<MapManager>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<PathFinder>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<ObjectPool>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<ButtonManager>().AsSingle();
+        Container.BindInterfacesAndSelfTo<GUIController>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<LevelManager>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<WaveManager>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<BuildManager>().AsSingle().NonLazy();
+        Container.Bind<MouseManager>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<GameManager>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<HoverManager>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<BuildValidator>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<Wallet>().AsSingle().NonLazy();
+        Container.BindInterfacesAndSelfTo<MessageSystem>().AsSingle().NonLazy();
+        Container.Bind<EffectParserJSON>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+        Container.Bind<TooltipManager>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+        Container.Bind<InputHandler>().FromNewComponentOnNewGameObject().AsSingle().NonLazy();
+
+        //  Misc bindings
+        Container.Bind<RadiusRenderer>().FromComponentInHierarchy().AsSingle();
+        Container.Bind<PathRenderer>().FromComponentInHierarchy().AsSingle();
+        Container.BindInterfacesAndSelfTo<StatusPanel>().AsSingle();
+        Container.BindInterfacesAndSelfTo<TowerPanel>().AsSingle();
+        Container.BindInterfacesAndSelfTo<TargetManager>().AsSingle();
+        Container.Bind<ToolTip>().FromComponentInHierarchy().AsSingle();
+        Container.BindInterfacesAndSelfTo<FPSCounter>().AsSingle();
+        Container.BindInterfacesAndSelfTo<ControlsPanel>().AsSingle();
+
+        //  Factory bindings
+        Container.BindIFactory<Enemy, Enemy.Factory>();
+        Container.BindFactory<Tower, Tower.Factory>();
+    }
+}
