992824426516dfd0bf8883c0a78ffb773a6ffc40
Thomas <thomas.sinka@dcmail.ca>
2021-07-11 02:41:49 -0400

    updated previewpathrenderer to use buildvalidator to ensure position is buildable when generating preview path in build mode, and demolishable when demolishing. also made it so preview path clears after user builds or exits edit mode

--- Assets/Scripts/PreviewPathRenderer.cs
+++ Assets/Scripts/PreviewPathRenderer.cs
@@ -5,34 +5,48 @@ using Zenject;
 
 public class PreviewPathRenderer : MonoBehaviour, IPathRenderer {
     [Inject] BuildManager buildManager;
+    [Inject] IBuildValidator buildValidator;
+
     IPathfinder pathFinder;
     IMapManager mapManager;
     private LineRenderer line;
     private readonly Vector3 tilemapOffset = new Vector3(0.5f, 0.5f, 0);
 
-
-    private void OnEnable() {
-        MouseManager.OnHoveredNewTile += HandleNewTileHovered;
+    private void Awake() {
         pathFinder = GameObject.Find("PathFinder").GetComponent<IPathfinder>();
         mapManager = GameObject.Find("MapManager").GetComponent<IMapManager>();
         line = GetComponent<LineRenderer>();
     }
 
+    private void OnEnable() {
+        MouseManager.OnHoveredNewTile += HandleNewTileHovered;
+        MouseManager.OnLeftMouseUp += ClearPath;
+        MouseManager.OnRightMouseUp += ClearPath;
+    }
+
     private void OnDisable() {
         MouseManager.OnHoveredNewTile -= HandleNewTileHovered;
+        MouseManager.OnLeftMouseUp -= ClearPath;
+        MouseManager.OnRightMouseUp += ClearPath;
     }
 
     private void HandleNewTileHovered(Vector3Int tileCoords) {
         if (buildManager.CurrentBuildMode == BuildManager.BuildMode.Build) {
-            if (pathFinder.IsOnMainPath(tileCoords)) {
+            if (pathFinder.IsOnMainPath(tileCoords) && buildValidator.IsPositionBuildable(tileCoords)) {
                 List<Vector3Int> path = pathFinder.GetBuildPreviewPath(tileCoords);
                 RenderPath(path);
             }
+            else {
+                ClearPath();
+            }
         }
         else if (buildManager.CurrentBuildMode == BuildManager.BuildMode.Demolish) {
-            if (mapManager.ContainsTileAt(IMapManager.Layer.StructureLayer, tileCoords)) {
+            if (buildValidator.CanDemolishStructure(tileCoords)) {
                 List<Vector3Int> path = pathFinder.GetDemolishPreviewPath(tileCoords);
                 RenderPath(path);
+            }
+            else {
+                ClearPath();
             }
         }
     }
@@ -43,5 +57,9 @@ public class PreviewPathRenderer : MonoBehaviour, IPathRenderer {
         for (int i = 0; i < path.Count; i++) {
             line.SetPosition(i, path[i] + tilemapOffset);
         }
+    }
+
+    public void ClearPath() {
+        line.positionCount = 0;
     }
 }
