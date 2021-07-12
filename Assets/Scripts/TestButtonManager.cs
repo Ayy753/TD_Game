d393f58f1fd432fb1aaa19fc0bcdede51a0e561b
Thomas <thomas.sinka@dcmail.ca>
2021-07-12 02:56:25 -0400

    bound test environment buttons to their respective function calls via testbuttonmanager

--- Assets/Scripts/TestButtonManager.cs
+++ Assets/Scripts/TestButtonManager.cs
@@ -12,10 +12,12 @@ public class TestButtonManager : ButtonManager, IInitializable {
     private IWaveManager waveManager;
     private GameManager gameManager;
     private EnemySpawner enemySpawner;
+    private IWallet wallet;
 
-    public TestButtonManager(BuildManager buildManager, IWaveManager waveManager, GameManager gameManager, EnemySpawner enemySpawner):base(buildManager, waveManager, gameManager) {
+    public TestButtonManager(BuildManager buildManager, IWaveManager waveManager, GameManager gameManager, EnemySpawner enemySpawner, IWallet wallet):base(buildManager, waveManager, gameManager) {
         Debug.Log("test build manager");
         this.enemySpawner = enemySpawner;
+        this.wallet = wallet;
     }
 
     public new void Initialize() {
@@ -24,7 +26,13 @@ public class TestButtonManager : ButtonManager, IInitializable {
     }
 
     public new void BindButtonsInScene() {
-        Button btnSpawn = GameObject.Find("btnSpawnEnemy").GetComponent<Button>();
-        btnSpawn.onClick.AddListener(delegate { enemySpawner.SpawnEnemy(EnemyData.Type.Normal); });
+        Button btnSpawnFast = GameObject.Find("btnSpawnFastEnemy").GetComponent<Button>();
+        Button btnSpawnNormal = GameObject.Find("btnSpawnNormalEnemy").GetComponent<Button>();
+        Button btnSpawnStrong = GameObject.Find("btnSpawnStrongEnemy").GetComponent<Button>();
+        Button btnAddGold = GameObject.Find("btnAddGold").GetComponent<Button>();
+        btnSpawnFast.onClick.AddListener(delegate { enemySpawner.SpawnEnemy(EnemyData.Type.Fast); });
+        btnSpawnNormal.onClick.AddListener(delegate { enemySpawner.SpawnEnemy(EnemyData.Type.Normal); });
+        btnSpawnStrong.onClick.AddListener(delegate { enemySpawner.SpawnEnemy(EnemyData.Type.Strong); });
+        btnAddGold.onClick.AddListener(delegate { wallet.GainMoney(100); });
     }
 }
