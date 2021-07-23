8987d5a7dae3eac3e537e70edbab060d6ac58745
Thomas <thomas.sinka@dcmail.ca>
2021-07-23 18:03:39 -0400

    added icon property to enemydata

--- Assets/Scripts/EnemyData.cs
+++ Assets/Scripts/EnemyData.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy Data")]
-public class EnemyData : CharacterData
-{
+public class EnemyData : CharacterData {
     [field: SerializeField] public int BaseValue { get; private set; }
     [field: SerializeField] public string Name { get; private set; }
     [field: SerializeField] public string Description { get; private set; }
     [field: SerializeField] public EnemyType Type { get; private set; }
-
+    [field: SerializeField] public Sprite Icon { get; private set; }
     public enum EnemyType {
         Fast,
         Normal,
