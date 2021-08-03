325ec935a68dd2c4e612003adf61e64c1435d53c
Thomas <thomas.sinka@dcmail.ca>
2021-08-02 23:54:23 -0400

    Refactored Projectile logic

--- Assets/Scripts/Projectiles/Projectile.cs
+++ Assets/Scripts/Projectiles/Projectile.cs
@@ -1,58 +1,58 @@
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-    public Transform Target { get; private set; }
-    public Vector3 LastTargetPosition { get; private set; }
+    private Transform target;
+    private Vector3 lastTargetPosition;
     private EffectGroup effectGroup;
-
-    private bool targetDied;
+    private bool targetDestroyed;
     private bool dealtDamage;
+    private const float PROJECTILE_SPEED = 5F;
 
     private void Update() {
-        MoveTowardsTarget();
+        MoveTowardsDestination();
     }
 
     public void Initialize(Vector3 startPos, Transform target, EffectGroup effectGroup) {
         transform.position = startPos;
-        Target = target;
-        targetDied = false;
+        this.target = target;
+        targetDestroyed = false;
         dealtDamage = false;
         this.effectGroup = effectGroup;
     }
 
-    protected void MoveTowardsTarget() {
-        if (Target.gameObject.activeInHierarchy == false) {
-            //  Make sure projectile doesn't start going after an enemy that respawned after dying
-            targetDied = true;
+    private void MoveTowardsDestination() {
+        if (target.gameObject.activeInHierarchy == false) {
+            targetDestroyed = true;
         }
 
-        if (targetDied != true) {
-            LastTargetPosition = Target.position;
-            transform.position = Vector3.MoveTowards(transform.position, Target.position, 5f * Time.deltaTime);
+        if (targetDestroyed != true) {
+            PursueTargetAndRecordLastPosition();
         }
         else {
-            transform.position = Vector3.MoveTowards(transform.position, LastTargetPosition, 5f * Time.deltaTime);
-            if (transform.position == LastTargetPosition)
-                gameObject.SetActive(false);
+            MoveToLastTargetPosition();
         }
     }
 
-    protected void OnTriggerEnter2D(Collider2D collision) {
+    private void PursueTargetAndRecordLastPosition() {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, PROJECTILE_SPEED * Time.deltaTime);
+        lastTargetPosition = target.position;
+    }
+
+    private void MoveToLastTargetPosition() {
+        transform.position = Vector3.MoveTowards(transform.position, lastTargetPosition, PROJECTILE_SPEED * Time.deltaTime);
+        if (transform.position == lastTargetPosition)
+            gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
         IEffectable effectable = collision.GetComponentInChildren<IEffectable>();
 
         //  If it hit a unit, and hasn't damaged anything yet (to prevent it from damaging multiple units at the same time)
         if (effectable != null && dealtDamage == false) {
-
-            //  If the unit is the target
-            if (effectable.GetTransform() == Target) {
+            //  If the unit is the target, or the target died and something else got hit
+            if (effectable.GetTransform() == target || targetDestroyed == true) {
                 dealtDamage = true;
                 ApplyEffectGroup(effectable);
-                gameObject.SetActive(false);
-            }
-            //  If target is already dead, it can damage another unit
-            else if (targetDied == true) {
-                ApplyEffectGroup(effectable);
-                dealtDamage = true;
                 gameObject.SetActive(false);
             }
         }
