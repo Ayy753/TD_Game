03a48d6c990b2b51ae81bcbd8f3af60c37b503ab
Thomas <thomas.sinka@dcmail.ca>
2021-08-03 21:17:48 -0400

    fixed newly introduced bug in Totem class where it applied the area effect group for each enemy in range

--- Assets/Scripts/Totem.cs
+++ Assets/Scripts/Totem.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 /// <summary>
 /// Applies positive effects to enemies within range
 /// </summary>
-public class Totem : MonoBehaviour, IEffectableRangeDetection, Itargetable {
+public class Totem : MonoBehaviour, Itargetable {
     public float Radius { get { return totemData.EffectGroup.Radius; } }
     [field: SerializeField] public TotemData totemData { get; private set; }
 
@@ -35,29 +35,12 @@ public class Totem : MonoBehaviour, IEffectableRangeDetection, Itargetable {
     }
 
     private void ApplyEffects() {
-        effectableOjbectsInRange = GetEffectableObjectsInRange(transform.position);
-        foreach (IEffectable effectable in effectableOjbectsInRange) {
-            totemData.EffectGroup.EffectArea(transform.position);
-        }
+        totemData.EffectGroup.EffectArea(transform.position);
     }
 
     //  Can't use DI on objects created at runtime
     public void Initialize(RadiusRenderer radiusRenderer) {
         this.radiusRenderer = radiusRenderer;
-    }
-
-    public List<IEffectable> GetEffectableObjectsInRange(Vector3 center) {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(center, Radius);
-        List<IEffectable> EffectableObjectsInRange = new List<IEffectable>();
-
-        foreach (var collider in colliders) {
-            IEffectable effectable = collider.GetComponent<IEffectable>();
-            if (effectable != null) {
-                EffectableObjectsInRange.Add(effectable);
-            }
-        }
-
-        return EffectableObjectsInRange;
     }
 
     void OnMouseOver() {
