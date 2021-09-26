eccad9698356c91e1f47f64701644a93729e9cdb
Thomas <thomas.sinka@dcmail.ca>
2021-09-26 01:04:59 -0400

    Created CharacterData test setup

--- Assets/Tests/StatusTests.cs
+++ Assets/Tests/StatusTests.cs
@@ -8,18 +8,30 @@ namespace Tests {
     using NUnit.Framework;
     using UnityEngine;
 
+    [TestFixture]
     public class StatusTests {
+        private CharacterData characterData;
+
+        [SetUp]
+        public void Init() {
+            characterData = (CharacterData)ScriptableObject.CreateInstance(typeof(CharacterData));
+            typeof(CharacterData).GetProperty(nameof(characterData.BaseHealth)).SetValue(characterData, 100);
+            typeof(CharacterData).GetProperty(nameof(characterData.BaseArmor)).SetValue(characterData, 20);
+            typeof(CharacterData).GetProperty(nameof(characterData.BaseColdResist)).SetValue(characterData, 10);
+            typeof(CharacterData).GetProperty(nameof(characterData.BaseFireResist)).SetValue(characterData, 10);
+            typeof(CharacterData).GetProperty(nameof(characterData.BaseLightningResist)).SetValue(characterData, 10);
+            typeof(CharacterData).GetProperty(nameof(characterData.BasePoisonResist)).SetValue(characterData, 10);
+            typeof(CharacterData).GetProperty(nameof(characterData.BaseSpeed)).SetValue(characterData, 10);
+        }
+
         [Test]
         public void Health_after_dealing_5_physical_damage_against_unit_with_20_armor_and_100_health_is_96() {
-            CharacterData characterData = (CharacterData)ScriptableObject.CreateInstance(typeof(CharacterData));
-            typeof(CharacterData).GetProperty(nameof(characterData.BaseArmor)).SetValue(characterData, 20);
-            typeof(CharacterData).GetProperty(nameof(characterData.BaseHealth)).SetValue(characterData, 100);
             IEffectable effectable = CreateMockEffectable(characterData);
             Damage damage = new Damage(5, DamageType.Physical);
             EffectGroup effectGroup = CreateMockEffectGroup(new IEffect[] { damage });
 
             effectGroup.EffectTarget(effectable);
-            
+
             Assert.AreEqual(96, effectable.Status.Health.Value);
         }
 
